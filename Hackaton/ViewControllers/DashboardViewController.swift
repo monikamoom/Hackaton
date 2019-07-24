152488ecceb477f9ec6f9387473cde531004779e
papob boonpat <Strong2540@hotmasil.com>
2019-07-25 01:16:28 +0700

    Add Navigation Dashboard element Profile Page

--- Hackaton/ViewControllers/DashboardViewController.swift
+++ Hackaton/ViewControllers/DashboardViewController.swift
@@ -1,30 +1,57 @@
-//
-//  DashboardViewController.swift
-//  Hackaton
-//
-//  Created by papob boonpat on 25/7/2562 BE.
-//  Copyright © 2562 s89506. All rights reserved.
-//
+
 
 import UIKit
 
 class DashboardViewController: UIViewController {
 
+    @IBOutlet weak var balanceView: UIView!
+    @IBOutlet weak var tranView: UIView!
+    @IBOutlet weak var V1: UIView!
+    @IBOutlet weak var V2: UIView!
+    
+    @IBOutlet weak var income: UIView!
+    @IBOutlet weak var expense: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        showIncomeGraph()
+        tranView.dropShadow()
+        balanceView.dropShadow()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func change(_ sender: UIButton) {
+        changeGrpah(sender.titleLabel?.text)
+        
     }
-    */
 
+}
+
+extension DashboardViewController{
+    
+    func showIncomeGraph(){
+        V1.isHidden = false
+        V2.isHidden = true
+        V1.dropShadow()
+        income.backgroundColor = #colorLiteral(red: 0.1529411765, green: 0.6823529412, blue: 0.3764705882, alpha: 1)
+        expense.backgroundColor = #colorLiteral(red: 1, green: 0.5472803116, blue: 0.4751736522, alpha: 1)
+    }
+    
+    func showExpenseGrap(){
+        V1.isHidden = true
+        V2.isHidden = false
+        V2.dropShadow()
+        income.backgroundColor = #colorLiteral(red: 0.1704154909, green: 0.8661643267, blue: 0.487077415, alpha: 1)
+        expense.backgroundColor = #colorLiteral(red: 0.9058823529, green: 0.2980392157, blue: 0.2352941176, alpha: 1)
+    }
+    
+    func changeGrpah(_ page:String!){
+        switch page {
+        case "income":
+            showIncomeGraph()
+        case "expense":
+            showExpenseGrap()
+        default:
+            break
+        }
+        
+    }
 }

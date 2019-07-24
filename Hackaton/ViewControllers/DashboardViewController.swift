3498df5a03660bc264219b2efee35c3ca8ca47b3
papob boonpat <Strong2540@hotmasil.com>
2019-07-25 00:48:21 +0700

    Create Tab bar and Switch page

---
+++ Hackaton/ViewControllers/DashboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DashboardViewController.swift
+//  Hackaton
+//
+//  Created by papob boonpat on 25/7/2562 BE.
+//  Copyright © 2562 s89506. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
